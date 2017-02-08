a7a9ce77912bb2a417b90b5b585626ecfecb3553
Ivan Moore <ivan@teamoptimization.com>
2017-02-08 23:26:28 +0000

    added files generated by stack

--- src/something.hs
+++ src/something.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Something where
+
+import Control.Applicative ((<$>), optional)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Data.Text.Lazy (unpack)
+import Happstack.Lite
+import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
+import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
 main = putStrLn "hello"
