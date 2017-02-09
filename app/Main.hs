bdc100f3a387388fcdab3492f27c097a4f98d671
Rob <robmoore121@gmail.com>
2017-02-09 23:30:47 +0000

    Can now get specified form-data from a POST to the server (such as before and after hashes from a github webhooks push event POST)

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,19 @@ module Main where
 
 import Happstack.Lite
 import Data.Maybe
+import Data.Text (Text)
+import Data.Text.Lazy (unpack)
 
 main :: IO ()
 main = serve Nothing gitListener
 
 gitListener :: ServerPart Response
-gitListener = ok $ toResponse "hello world\n"
+gitListener = do
+  before <- formdataField "before"
+  after  <- formdataField "after"
+  ok $ toResponse ("after_data: " ++ after ++ "\nbefore_data: " ++ before ++ "\n")
+
+formdataField :: String -> ServerPart String
+formdataField fieldName = do
+  msg <- lookText fieldName
+  return (unpack msg)
