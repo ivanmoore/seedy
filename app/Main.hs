9cb7a720cf4632af70a0cc0de9cf5c05c700bcdd
Rob <robmoore121@gmail.com>
2017-02-09 22:46:14 +0000

    Removed many unnecassary imports, got a simple, working, hello world webapp which runs on localhost8000

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,10 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Main where
 
-import Lib
-
-import Control.Applicative ((<$>), optional)
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
-import Data.Text.Lazy (unpack)
 import Happstack.Lite
-import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
-import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
+import Data.Maybe
 
 main :: IO ()
-main = do
-    putStrLn "hello"
-    someFunc
+main = serve Nothing gitListener
+
+gitListener :: ServerPart Response
+gitListener = ok $ toResponse "hello world\n"
