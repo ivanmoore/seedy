d4127319f0812e2bdd63b4c9f2bed202437dbe9c
Ivan Moore <ivan@teamoptimization.com>
2017-02-11 12:07:25 +0000

    tried to do some programming but failed

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,7 @@
 module Main where
 
 import Happstack.Lite
-import Data.Maybe
-import Data.Text (Text)
-import Data.Text.Lazy (unpack)
+import Lib
 
 main :: IO ()
-main = serve Nothing gitListener
-
-gitListener :: ServerPart Response
-gitListener = do
-  before <- formdataField "before"
-  after  <- formdataField "after"
-  ok $ toResponse ("after_data: " ++ after ++ "\nbefore_data: " ++ before ++ "\n")
-
-formdataField :: String -> ServerPart String
-formdataField fieldName = do
-  msg <- lookText fieldName
-  return (unpack msg)
+main = putStr "serve Nothing gitListener"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Spec where
+
+import Lib
+
+tests :: IO ()
+tests = putStrLn (show canParseCommits)
+
+canParseCommits :: Bool
+canParseCommits = 59 == (length . parseCommits) "https://api.github.com/repos/ivanmoore/Eatcheap/commits"
