5bc98d352fc01b2a0c9b330d39c3fbfb7d4d8430
Rob <robmoore121@gmail.com>
2017-06-24 12:50:36 +0100

    Updated test to be what we actually want. Accesses the network, so quite slow

--- src/JSONHandler.hs
+++ src/JSONHandler.hs
@@ -4,7 +4,6 @@ import Pelias
 import SeedyPelias
 
 latestCommitAuthor :: String -> IO String
-latestCommitAuthor filename = do
-  json                   <- readFile filename
+latestCommitAuthor json = do
   (Just (SValue author)) <- pure $ seedyExtract [Index 0, Get "author", Get "login"] json
   return author
--- test/EndToEndTest.hs
+++ test/EndToEndTest.hs
@@ -1,10 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module EndToEndTest where
+
+import Network.HTTP.Client
+import Network.HTTP.Simple
+import Network.HTTP.Types.Header
+import Data.ByteString.Lazy.Char8 as C8
 
 import RobUnit
 import JSONHandler
 
 main :: IO ()
 main = do
-  putStrLn "=== END TO END ==="
-  ioResult $ ioTest "can get first committer username" "robknows" (latestCommitAuthor "resources/eatcheap.json")      
-  putStrLn "ALL DONE"
+  Prelude.putStrLn "=== END TO END ==="
+
+  req  <- parseRequest "https://api.github.com/repos/ivanmoore/Eatcheap/commits"
+  resp <- httpLBS (req {requestHeaders = [(hUserAgent, "Haskell")]})
+  json <- pure $ C8.unpack $ responseBody resp
+
+  ioResult $ ioTest "can get first committer username" "robknows" (latestCommitAuthor json)
+
+  Prelude.putStrLn "ALL DONE"
